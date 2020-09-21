08ec069331fa3d00f4d2052ca90079890f705542
Nick Bindass <natasa91@rediffmail.com>
2020-09-21 20:41:03 +0530

    test

--- src/routes/example.router.ts
+++ src/routes/example.router.ts
@@ -6,7 +6,7 @@ router.get(
   "/test/:name",
   (req: Request, res: Response, next: NextFunction) => {
     const { name } = req.params;
-    res.send(`Hello ${name} final ********`);
+    res.send(`Hello ${name}abcd`);
   }
 );
 
