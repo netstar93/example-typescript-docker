14afffe40dcb9ac024d35e4e2aab26d581ddc962
Nick Bindass <natasa91@rediffmail.com>
2020-09-21 13:39:00 +0530

    test

--- src/routes/example.router.ts
+++ src/routes/example.router.ts
@@ -6,7 +6,7 @@ router.get(
   "/test/:name",
   (req: Request, res: Response, next: NextFunction) => {
     const { name } = req.params;
-    res.send(`Hello ${name} from express test`);
+    res.send(`Hello ${name} from express how are you`);
   }
 );
 
