470966882dad019c035d3113aac481664ef40228
Nick Bindass <natasa91@rediffmail.com>
2020-09-21 16:44:08 +0530

    test

--- src/routes/example.router.ts
+++ src/routes/example.router.ts
@@ -1,12 +1,12 @@
 import { Router, Request, Response, NextFunction } from "express";
-import * as mongoose from 'mongoose';
+import * as mongoose from 'nconf';
 const router = Router();
 
 router.get(
   "/test/:name",
   (req: Request, res: Response, next: NextFunction) => {
     const { name } = req.params;
-    res.send(`Hello ${name} from final remove2222222222`);
+    res.send(`Hello ${name} from`);
   }
 );
 
