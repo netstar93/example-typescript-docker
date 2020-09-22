a5f5cf1259ef4a5f60f55701aa47ed8bdfa90e2d
Nick Bindass <natasa91@rediffmail.com>
2020-09-22 09:24:56 +0530

    test

--- src/routes/example.router.ts
+++ src/routes/example.router.ts
@@ -6,7 +6,7 @@ router.get(
   "/test/:name",
   (req: Request, res: Response, next: NextFunction) => {
     const { name } = req.params;
-    res.send(`Hello ${name} with mongoose with email 2`);
+    res.send(`Hello ${name} with mongoose with email 2s`);
   }
 );
 
